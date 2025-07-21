709036c4444d134dc6db7ceca91bb5d75a06cce9
mukherjeeuttio <mukherjeeuttio@gmail.com>
2025-07-22 00:47:24 +0530

    Revert "basic file upload backend added"
    This reverts commit 906e98ca5b3576dcb85dad17bac5d8a779ad011f.

--- src/main/java/com/docuintel/docuintel_core/repository/FileMetaDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.docuintel.docuintel_core.repository;
-
-import com.docuintel.docuintel_core.entity.FileMetaData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileMetaDataRepository extends JpaRepository<FileMetaData, Long> {
-}
