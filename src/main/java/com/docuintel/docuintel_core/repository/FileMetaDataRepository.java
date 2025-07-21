906e98ca5b3576dcb85dad17bac5d8a779ad011f
mukherjeeuttio <mukherjeeuttio@gmail.com>
2025-07-22 00:44:18 +0530

    basic file upload backend added

---
+++ src/main/java/com/docuintel/docuintel_core/repository/FileMetaDataRepository.java
@@ -0,0 +1,7 @@
+package com.docuintel.docuintel_core.repository;
+
+import com.docuintel.docuintel_core.entity.FileMetaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileMetaDataRepository extends JpaRepository<FileMetaData, Long> {
+}
