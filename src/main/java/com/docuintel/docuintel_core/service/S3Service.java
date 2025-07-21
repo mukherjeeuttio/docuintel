84a0364cc8553ad9578a91216cc4237782e0dc34
mukherjeeuttio <mukherjeeuttio@gmail.com>
2025-07-22 00:58:00 +0530

    added missing files

---
+++ src/main/java/com/docuintel/docuintel_core/service/S3Service.java
@@ -0,0 +1,32 @@
+package com.docuintel.docuintel_core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+
+    @Autowired
+    private S3Client s3Client;
+    private final String bucketName = "docuintel-uttio-2025";
+
+    public void uploadFile(String key, byte[] fileBytes) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+        s3Client.putObject(request, RequestBody.fromBytes(fileBytes));
+    }
+
+    public void deleteFile(String key) {
+        DeleteObjectRequest request = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+        s3Client.deleteObject(request);
+    }
+}
